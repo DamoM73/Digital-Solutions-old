481229cc6c2f92debf5240e2320b8206599d7bb0
Damien <damomurtagh@gmail.com>
2020-04-23 21:46:49 +1000

    Finished prime exercise

--- Unit 1/prime_numbers_ex_2b.py
+++ Unit 1/prime_numbers_ex_2b.py
@@ -28,14 +28,59 @@ def find_next_prime(start):
         else:
             next_num += 1
 
-# ----- Create UI -----
+def start_cmd():
+    global running
+    running = True
+
+def stop_cmd():
+    global running
+    running = False
+
+def reset_cmd():
+    global running
+    global highest_prime
+    running = False
+    highest_prime = 0
+    num_label.config(text=str(highest_prime))
+    root.update
+
+# ----- CREATE UI -----
+# -- create window --
 root = tk.Tk()
-root.geometry("800x600")
+root.geometry("600x400")
+root.title("Prime Numbers")
 
+# -- add window content --
+# heading label
+tk.Label(root, text="Prime Numbers", font=("Helvetica", "30")).pack(expand="Y")
 
-# ----- Main Program -----
+# number label
+num_label = tk.Label(root, text="0", font=("Helvetica", "60"))
+num_label.pack(expand="Y")
+
+# button frame
+btn_frame = tk.Frame(root)
+btn_frame.pack(expand="Y")
+
+# buttons
+start_btn = tk.Button(btn_frame, text="Start", font=("Helvetica", "20"), width=10, bg="GREEN", command=start_cmd)
+start_btn.grid(row=0,column=0,padx=10)
+
+stop_btn = tk.Button(btn_frame,text = "Stop", font=("Helvetica", "20"), width=10, bg="RED", command=stop_cmd)
+stop_btn.grid(row=0,column=1,padx=10)
+
+reset_btn = tk.Button(btn_frame, text = "Reset", font=("Helvetica", "20"), width=10, bg="ORANGE", comman=reset_cmd)
+reset_btn.grid(row=0,column=2,padx=10)
+
+# ----- MAIN PROGRAM -----
 highest_prime = 0
+running = False
 
 while True:
-    highest_prime = find_next_prime(highest_prime)
-    print(highest_prime)
+     if running:
+        highest_prime = find_next_prime(highest_prime)
+        print(highest_prime)
+        num_label.config(text=str(highest_prime))
+     root.update()
+
+#root.mainloop()
