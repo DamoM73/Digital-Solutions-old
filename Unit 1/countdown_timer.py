6392f1aed61fc98dffd0406a22fdb14b31e7eae5
Damien Murtagh <MurtaghD@mbbc.qld.edu.au>
2020-05-12 14:12:13 +1000

    timer

--- Unit 1/countdown_timer.py
+++ Unit 1/countdown_timer.py
@@ -1,6 +1,16 @@
 import time
 
-seconds = 90
+time_remaining = 90
 
-while seconds > 0:
+while time_remaining > 0:
+    minutes = str(time_remaining // 60)
+    seconds = time_remaining % 60
+    if seconds < 10:
+        seconds = "0"+str(seconds)
+    else:
+        seconds = str(seconds)
+    print(minutes+":"+seconds)
+    time_remaining -= 1
+    time.sleep(1)
+
     
