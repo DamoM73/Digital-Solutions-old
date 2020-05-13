4dc04ffd942cca2fc1f68f5493168262a9b4fe89
Damien Murtagh <MurtaghD@mbbc.qld.edu.au>
2020-05-13 10:20:50 +1000

    stop watch start

---
+++ Unit 1/stop_watch.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+# ***** GLOBAL VARIABLES *****
+current_time = 0
+
+def time_to_str(time_val):
+    # converts time into H:MM:SS:mm format
+    
+
+
+
+# ***** CREATE WINDOW *****
+root = tk.Tk()
+root.title("Stop Watch")
+
+# *** Window content ***
+
+
+
+# **** MAIN PROGRAM ****
+root.mainloop()
