7fa372cf6698a75acac971d3b10357b790daf682
DamoM73 <damomurtagh@gmail.com>
2020-05-18 13:42:33 +1000

    stuff

--- Unit 3/read_library_csv.py
+++ Unit 3/read_library_csv.py
@@ -2,19 +2,22 @@ import csv
 
 library_db = []
 
-with open('./Unit 3/full_resources.txt') as library_file:
+with open('./Unit 3/Library.txt') as library_file:
     csv_reader = csv.reader(library_file, delimiter='\t')
     line_count = 0
-    for item in library_file:
-        item = list(item.split('\t'))   
-        isbn = item[10]
-        title = item[21].strip('"')
-        book = item[26] == 'Monograph'
+    for item in csv_reader:
+        # print(item)
+        # item = list(item.split('\t'))   
+        # isbn = item[10]
+        # title = item[21].strip('"')
+        # book = item[26] == 'Monograph'
         author = item[32].strip('"')
         
-        if book:
-            library_db.append([isbn,author,title,])
+        print(author)
+
+        # if book:
+        #     library_db.append([isbn,author,title,])
     
-for book in library_db:
-    print(book)
+# for book in library_db:
+#     print(book)
 
