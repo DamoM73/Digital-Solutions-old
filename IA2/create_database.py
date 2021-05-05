35359e85b6f6cdd0ee372191216636a69060419e
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-05-05 17:54:06 +1000

    basic tables

--- IA2/create_database.py
+++ IA2/create_database.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql
+import csv
 import os
 
 def delete_db(db_file):
@@ -60,13 +61,18 @@ def create_db(db_file):
                             popularity INTEGER NOT NULL,
                             duration INTEGER NOT NULL,
                             explict INTERGER NOT NULL,
-                            released TEXT NOT NULL,
                             danceability REAL NOT NULL,
                             valence REAL NOT NULL,
                             year INTEGER NOT NULL,
                             accousticness REAL NOT NULL,
                             energy REAL NOT NULL,
-                            instrumentalness REAL NOT NULL
+                            instrumentalness REAL NOT NULL,
+                            key INTEGER NOT NULL,
+                            liveness REAL NOT NULL,
+                            loudness REAL NOT NULL,
+                            mode INTEGER NOT NULL,
+                            speechiness REAL NOT NULL,
+                            tempo REAL NOT NULL
                         );
                         """
     sql_command(db_file,create_song_tbl)
@@ -82,11 +88,78 @@ def create_db(db_file):
     sql_command(db_file,create_songartist_tbl)
 
 def import_db(db_file):
-    # import artist table
-    pass
+    # import artist data and genre data
+    with open(ARTIST_FILE, encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        next(csv_reader)    # skips header
+        # create sql command for each row / record
+        added_genres = []
+        for row in csv_reader:
+            # add artist information
+            id = row[0]
+            name = row[3].replace('"',"'")
+            insert_artist = f"""
+                            INSERT INTO Artist (artist_id, artist_name)
+                            VALUES ("{id}","{name}")
+                            """
+            sql_command(db_file,insert_artist)
+            print(f"Artist {name} added")
+
+            # add genre information
+            genres = row[2].strip('][').split(',')
+            for raw_genre in genres:
+                genre = raw_genre.strip()[1:-1]
+                if genre not in added_genres and genre != "":
+                    insert_genre = f"""
+                                    INSERT INTO Genre (genre_name)
+                                    VALUES ("{genre}")
+                                    """
+                    sql_command(db_file,insert_genre)
+                    added_genres.append(genre)
+                    print(f"Genre of {genre} added")
+    
+    # import song data
+    with open(DATA_FILE, encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        next(csv_reader)    # skips header
+        # create sql command for each row / record
+        for row in csv_reader:
+            id = row[8]
+            name = row[14].replace('"',"'")
+            pop = row[15]
+            duration = row[5]
+            explict = row[7]
+            dance = row[4]
+            vale = row[0]
+            year = row[1]
+            accoustic = row[2]
+            energy = row[6]
+            instrument = row[9]
+            key = row[10]
+            live = row[11]
+            loud = row[12]
+            mode = row[13]
+            speech = row[17]
+            tempo = row[18]
+            
+            insert_songs = f"""
+                            INSERT INTO Songs
+                            VALUES ("{id}","{name}",{pop},{duration},{explict},{dance},{vale},
+                            {year},{accoustic},{energy},{instrument},{key},{live},{loud},{mode},{speech},{tempo})
+                            """
+                
+            sql_command(db_file,insert_songs)
+            print(f"Song {name} added")
+            
+
+
+
+
 
 # ----- MAIN PROGRAM -----
 DB_FILE = "./IA2/spotify.db"
+ARTIST_FILE = "./IA2/artists.csv"
+DATA_FILE = "./IA2/data_o.csv"
 
 delete_db(DB_FILE)
 create_db(DB_FILE)
